df7ab60d1ef6a025f5bf044e979194463e61b4bb
Jules <julia.krasnitski@gmail.com>
2023-03-09 18:13:04 -0500

    add FindFirstNonRepeatingCharacter

---
+++ src/test/java/string/FindFirstNonRepeatingCharacter.java
@@ -0,0 +1,35 @@
+package string;
+
+import java.util.*;
+
+public class FindFirstNonRepeatingCharacter {
+    public static void main(String[] args) {
+        String str = "aaabccc";
+        char[] arr = str.toCharArray();
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        //find unique character
+        HashSet<Character> set = new HashSet<>();
+        
+        for (int i=0; i<arr.length; i++){
+            char ch = str.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch, map.get(ch)+1);
+            }else {
+                map.put(ch, 1);
+                set.add(ch);
+            }
+        }
+
+        char res = '_';
+        Iterator itr = set.iterator();
+        while (itr.hasNext()) {
+            char ch = (char) itr.next();
+            if(map.get(ch)==1){
+                res = ch;
+            }
+        }
+
+        System.out.println(res);
+    }
+}
