b8281b44473feffa32d06ba2e1790c9e0ef344c0
Jules <julia.krasnitski@gmail.com>
2023-03-30 09:48:40 -0400

    add FindLongestPolidromicSubstring

--- src/test/java/string/FindLongestPolidromicSubstring.java
+++ src/test/java/string/FindLongestPolidromicSubstring.java
@@ -17,54 +17,9 @@ public class FindLongestPolidromicSubstring {
     //Input: s = "cbbd"
     //Output: "bb"
 
-    /*public static void main(String[] args) {
-            String str = "cbbd";//"bababad";//"babad";
-            int[] index = new int[2];
-
-            for(int i=str.length()-1, e=0; i>=0; i--){
-                //save first temp char
-                char temp = str.charAt(e);
-
-                if(temp == str.charAt(i)){
-                    index[0] = e;
-                    index[1] = i;
-                }else if (temp != str.charAt(i) && i>=0){
-                    e++;
-                }
-            }
-
-            for(int i=index[0]; i<=index[1]; i++){
-                System.out.println(str.charAt(i));
-            }
-    }*/
-
-/*    public static void main(String[] args) {
-        String str = "cbhbd";//"bababad";//"babad";
-        int length = str.length()-2;
-        int[] index = new int[2];
-
-        if(length>2){
-            for(int i=1; i<=length;i++){
-                int left = i-1;
-                int rigth = i+1;
-                if(str.charAt(left)==str.charAt(rigth)){
-                    index[0] = left;
-                    index[1] = rigth;
-                }
-*//*                if(left > 0 || rigth < length){
-                    int subLeft = left-1;
-                    int subRigth = rigth+1;
-                }else{
-                    i++;
-                }*//*
-            }
-        }
-        Arrays.stream(index).forEach(System.out::println);
-    }*/
-
 
     public static void main(String[] args) {
-        String str = "babad";//"cbbd";//
+        String str = "babad";//"cbbd";
         String num = str.length()%2==0? "odd":"even";
         int left = 0;
         int right =0;
