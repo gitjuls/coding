4a6aeaf79b2c15ee7672bb4a3a335a5117bb3943
Jules <julia.krasnitski@gmail.com>
2023-04-13 22:36:38 -0400

    add RemovingStarsFromAString

--- src/test/java/string/RemovingStarsFromAString.java
+++ src/test/java/string/RemovingStarsFromAString.java
@@ -1,2 +1,38 @@
-package string;public class RemovingStarsFromAString {
+package string;
+
+public class RemovingStarsFromAString {
+    //You are given a string s, which contains stars *.
+    //In one operation, you can:
+    //Choose a star in s.
+    //Remove the closest non-star character to its left, as well as remove the star itself.
+    //Return the string after all stars have been removed.
+
+    //Example 1:
+    //Input: s = "leet**cod*e"
+    //Output: "lecoe"
+    //Explanation: Performing the removals from left to right:
+    //- The closest character to the 1st star is 't' in "leet**cod*e". s becomes "lee*cod*e".
+    //- The closest character to the 2nd star is 'e' in "lee*cod*e". s becomes "lecod*e".
+    //- The closest character to the 3rd star is 'd' in "lecod*e". s becomes "lecoe".
+    //There are no more stars, so we return "lecoe".
+    //
+    //Example 2:
+    //Input: s = "erase*****"
+    //Output: ""
+    //Explanation: The entire string is removed, so we return an empty string.
+
+    public static void main(String[] args) {
+        String str = "leet**cod*e"; //"erase*****";
+        StringBuilder sb = new StringBuilder(str);
+
+        do{
+            for (int i = 0, j = 1; j <= sb.length()-1; i++, j++) {
+               if (sb.charAt(i) != '*' && sb.charAt(j) == '*') {
+                        sb.deleteCharAt(j);
+                        sb.deleteCharAt(i);
+               }
+            }
+        }while(sb.chars().anyMatch(ch -> ch == '*'));
+        System.out.println(sb);
+    }
 }
