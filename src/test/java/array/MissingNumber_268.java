62fd053dd3a814e99b5acc3127e20e922c084c46
Jules <julia.krasnitski@gmail.com>
2023-04-16 23:03:49 -0400

    add MissingNumber_268

--- src/test/java/array/MissingNumber_268.java
+++ src/test/java/array/MissingNumber_268.java
@@ -1,9 +1,11 @@
 package array;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
-public class MissingNumber {
+public class MissingNumber_268 {
+    //https://leetcode.com/problems/missing-number/
     //Given an array nums containing n distinct numbers in the range [0, n],
     // return the only number in the range that is missing from the array.
 
@@ -26,17 +28,28 @@ public class MissingNumber {
     // 8 is the missing number in the range since it does not appear in nums.
 
     public static void main(String[] args) {
-        int[] arr = {3,0,1};//{9,6,4,2,3,5,7,0,1};//{0,1};
+        int[] arr = {0,2,3}; //{3, 0, 1};//{9,6,4,2,3,5,7,0,1};//{0,1};//{1};
         Arrays.sort(arr);
 
         int res = 0;
-        for(int i=0, e=1; e<arr.length; i++, e++){ //O(n)
-            boolean isTrue = arr[e] - arr[i] != 1;
-            if(isTrue){
-               res = arr[i]+1;       //save missing num
-            }else{
-                res = arr[e]+1;     //if no missing num => save last num + 1
+        if (arr.length > 1) {
+            for (int i = 0, e = 1; e < arr.length; i++, e++) { //O(n)
+                boolean isNull = arr[0] == 0;
+                boolean isTrue = arr[e] - arr[i] != 1;
+                if(!isNull){
+                    res=0; break;          //if first num is not 0, return missing 0
+                }else if (isTrue) {
+                    res = arr[i] + 1;       //save missing num
+                    break;
+                }else{
+                    res = arr[e] + 1;     //if no missing num => save last num + 1
+                }
+
             }
+        } else if (arr.length == 1 && arr[0] == 0) {
+            res = 1;
+        } else if (arr.length == 1 && arr[0] == 1) {
+            res = 0;
         }
         System.out.println(res);
     }
