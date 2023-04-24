35efe4099bfaee67ee0130d19d01e5913e580c77
Jules <julia.krasnitski@gmail.com>
2023-04-24 11:12:19 -0400

    add FindTheIndexOfTheFirstOccurrenceInString_28

--- src/test/java/string/FindTheIndexOfTheFirstOccurrenceInString_28.java
+++ src/test/java/string/FindTheIndexOfTheFirstOccurrenceInString_28.java
@@ -33,9 +33,9 @@ public class FindTheIndexOfTheFirstOccurrenceInString_28 {
                 //if 8-0>=3;  8-1>=3; 8-2>=3; ... 8-5>=3 true
                 //if true, verify last occurrence and break;
                 boolean isTrue = haystack.substring(i, len+i).equalsIgnoreCase(needle);
-                //                sadbutsad        0   3+0 sad                sad
-                //                                 1   3+1 adb
-                //                                 2   3+2 dbu
+                //                sadbutsad        0   3+0 sad                sad true
+                //                                 1   3+1 adb                    false
+                //                                 2   3+2 dbu                    false
                 if(isTrue){output = i; break;}
                 //find first occurrence and break;
             }
