7ce0f0a89d7200a13c443e6d48369c4b29fe6191
Jules <julia.krasnitski@gmail.com>
2023-07-18 19:22:41 -0400

    ProductOfArrayExceptSelf_238

--- src/test/java/array/ProductOfArrayExceptSelf_238.java
+++ src/test/java/array/ProductOfArrayExceptSelf_238.java
@@ -15,11 +15,11 @@ public class ProductOfArrayExceptSelf_238 {
     //Output: [0,0,9,0,0]
 
     public static void main(String[] args) {
-        int[] nums = {-1,1,0,-3,3};//{1,2,3,4};
+        int[] nums = {0,0};//{1,2,3,4}; //{-1,1,0,-3,3};
         int[] output = new int[nums.length];
 
         int a=1;
-        for(int pointer=0; pointer<=nums.length-1; pointer++){//O[n^2]
+/*        for(int pointer=0; pointer<=nums.length-1; pointer++){//O[n^2]
             for(int i=0; i<=nums.length-1; i++){
                 //p1
                 //p2
@@ -32,14 +32,40 @@ public class ProductOfArrayExceptSelf_238 {
                     int temp = nums[i];
                     a=a*temp;
                 }
-                // [0][0]=2  [0][1]=3  [0][2]=4
-                // [1][0]=1  [1][1]=3  [1][2]=4
-                // [2][0]=1  [2][1]=2  [2][2]=4
-                // [3][0]=1  [3][1]=2  [3][2]=3
+                // [0][0]=2  [0][1]=3  [0][2]=4  2*3*4=24
+                // [1][0]=1  [1][1]=3  [1][2]=4  1*3*4=12
+                // [2][0]=1  [2][1]=2  [2][2]=4  1*2*4=8
+                // [3][0]=1  [3][1]=2  [3][2]=3  1*2*3=6
             }
             output[pointer] = a;
             a=1;
-        }
+        }*/
+
+        int pointer = 0;
+            for(int i=0; i<=nums.length-1; i++){
+                //p1
+                //p2
+                //p3
+                //p4
+                if(pointer==i && i!=nums.length-1){    // increase "i" and skip num
+                    i++;
+                }
+                if(i!=pointer){                         // save num and multiply
+                    int temp = nums[i];
+                    a=a*temp;
+                }
+                // [0][0]=2  [0][1]=3  [0][2]=4  2*3*4=24
+                // [1][0]=1  [1][1]=3  [1][2]=4  1*3*4=12
+                // [2][0]=1  [2][1]=2  [2][2]=4  1*2*4=8
+                // [3][0]=1  [3][1]=2  [3][2]=3  1*2*3=6
+                if(pointer<=nums.length-1 && i==nums.length-1){
+                output[pointer] = Math.abs(a);
+                a=1;
+                pointer++;
+                i=0;
+                }
+            }
+
 
         for(int num:output){//O(n)
             System.out.println(num);
